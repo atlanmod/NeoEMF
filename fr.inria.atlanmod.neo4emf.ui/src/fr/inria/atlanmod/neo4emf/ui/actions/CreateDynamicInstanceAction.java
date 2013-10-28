eca94e2eefeefec9a1bad961e781a802208371d3
abelgomez <abel.gomez-llana@inria.fr>
2013-10-28 16:00:58 +0100

    MigrateAction has been refactored into MigrateCommand

--- fr.inria.atlanmod.neo4emf.ui/src/fr/inria/atlanmod/neo4emf/ui/actions/CreateDynamicInstanceAction.java
+++ fr.inria.atlanmod.neo4emf.ui/src/fr/inria/atlanmod/neo4emf/ui/actions/CreateDynamicInstanceAction.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Atlanmod INRIA LINA Mines Nantes
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
+ *******************************************************************************/
 package fr.inria.atlanmod.neo4emf.ui.actions;
 
 
@@ -20,6 +30,7 @@ import fr.inria.atlanmod.neo4emf.ui.wizards.DynamicModelWizard;
 
 /**
  * Create a dynamic instance of an {@link EClass}.
+ * @author abelgomez
  */
 public class CreateDynamicInstanceAction extends ActionDelegate implements IActionDelegate {
 	protected static final URI PLATFORM_RESOURCE = URI.createPlatformResourceURI("/", false);
