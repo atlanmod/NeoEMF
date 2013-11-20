c181733e7327ce3f0e07e231914d1c30f0706f4a
abelgomez <abel.gomez-llana@inria.fr>
2013-11-20 19:13:43 +0100

    Organize imports and reformat

--- fr.inria.atlanmod.neo4emf/src/fr/inria/atlanmod/neo4emf/drivers/impl/PersistenceServiceFactory.java
+++ fr.inria.atlanmod.neo4emf/src/fr/inria/atlanmod/neo4emf/drivers/impl/PersistenceServiceFactory.java
@@ -15,66 +15,65 @@ package fr.inria.atlanmod.neo4emf.drivers.impl;
 
 import java.util.Map;
 
-import org.eclipse.core.runtime.IStatus;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import fr.inria.atlanmod.neo4emf.drivers.IPersistenceManager;
 import fr.inria.atlanmod.neo4emf.drivers.IPersistenceService;
 import fr.inria.atlanmod.neo4emf.drivers.IPersistenceServiceFactory;
-import fr.inria.atlanmod.neo4emf.logger.Logger;
-
 
 public class PersistenceServiceFactory extends GraphDatabaseFactory implements IPersistenceServiceFactory {
 
-	@Override	
- public IPersistenceService createPersistenceService( String path, IPersistenceManager persistenceManager, Map<String,String> config ){
+	@Override
+	public IPersistenceService createPersistenceService(String path, IPersistenceManager persistenceManager, Map<String, String> config) {
 		IPersistenceService service = new PersistenceService(path, persistenceManager);
 		registerShutdownHook(service);
 		return service;
-				
+
 	}
-	public static IPersistenceServiceFactory init () {
-		if(eINSTANCE == null) {
-			return  new PersistenceServiceFactory();
+
+	public static IPersistenceServiceFactory init() {
+		if (eINSTANCE == null) {
+			return new PersistenceServiceFactory();
 		}
 		return eINSTANCE;
 	}
-	
-//	public PersistenceServiceFactory (){
-//		//the cache providers
-//	    ArrayList<CacheProvider> cacheList = new ArrayList<CacheProvider>();
-////	    cacheList.add( new GCResistantCacheProvider() );
-//
-//	    //the index providers
-//	    IndexProvider lucene = new LuceneIndexProvider();
-//	    ArrayList<IndexProvider> provs = new ArrayList<IndexProvider>();
-//	    provs.add( lucene );
-//	    ListIndexIterable providers = new ListIndexIterable();
-//	    providers.setIndexProviders( provs );
-//	    this.setIndexProviders( providers );
-//	    this.setCacheProviders( cacheList );
-//	}
-	
-	 /**
-     * Register a shutdown so the database shuts clearly when an exception is raised
-     * @param persistenceService {@link IPersistenceService}
-     */
-	private static void registerShutdownHook( final IPersistenceService graphDb )
-	{
 
-		Runtime.getRuntime().addShutdownHook( new Thread()
-		{
+	// public PersistenceServiceFactory (){
+	// //the cache providers
+	// ArrayList<CacheProvider> cacheList = new ArrayList<CacheProvider>();
+	// // cacheList.add( new GCResistantCacheProvider() );
+	//
+	// //the index providers
+	// IndexProvider lucene = new LuceneIndexProvider();
+	// ArrayList<IndexProvider> provs = new ArrayList<IndexProvider>();
+	// provs.add( lucene );
+	// ListIndexIterable providers = new ListIndexIterable();
+	// providers.setIndexProviders( provs );
+	// this.setIndexProviders( providers );
+	// this.setCacheProviders( cacheList );
+	// }
+
+	/**
+	 * Register a shutdown so the database shuts clearly when an exception is
+	 * raised
+	 * 
+	 * @param persistenceService
+	 *            {@link IPersistenceService}
+	 */
+	private static void registerShutdownHook(final IPersistenceService graphDb) {
+		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
-			public void run()
-			{
+			public void run() {
+				// At this point the plugin "Neo4emfPlugin" has been already stopped
+				// Don't use the shared instance!
+				// Shutdown the DB
 				graphDb.shutdown();
-				Logger.log(IStatus.INFO, "Shutdown hook ");
 			}
-		} );
+		});
 	}
+
 	@Override
-	public IPersistenceService createPersistenceService(String path,
-			IPersistenceManager persistenceManager) {
+	public IPersistenceService createPersistenceService(String path, IPersistenceManager persistenceManager) {
 		IPersistenceService service = new PersistenceService(path, persistenceManager);
 		registerShutdownHook(service);
 		return service;
