bd3eb35f9a1f061aac48cb3aaae9eb9a8c955261
gdaniel <zelus444@gmail.com>
2014-03-17 15:50:35 +0100

    cleaning

--- fr.inria.atlanmod.neo4emf/src/main/java/fr/inria/atlanmod/neo4emf/change/impl/NewObject.java
+++ fr.inria.atlanmod.neo4emf/src/main/java/fr/inria/atlanmod/neo4emf/change/impl/NewObject.java
@@ -1,9 +1,5 @@
 package fr.inria.atlanmod.neo4emf.change.impl;
 
-import java.util.Iterator;
-
-import org.eclipse.emf.ecore.EStructuralFeature;
-
 import fr.inria.atlanmod.neo4emf.INeo4emfObject;
 import fr.inria.atlanmod.neo4emf.drivers.impl.Serializer;
 
@@ -13,28 +9,8 @@ import fr.inria.atlanmod.neo4emf.drivers.impl.Serializer;
  */
 public class NewObject extends Entry {
 	
-	/**
-	 * TODO ToFix, may be fixed with the proper solution to handle
-	 * resource.add(..) at the end of the execution (= a way to store 
-	 * properly strong references when there is no ChangeLog accessible from
-	 * a given object).
-	 * 
-	 * In that case the idea is to store strong references at the creation of the
-	 * object and allow the changelog to remove those references when the objects are
-	 * saved (accessible from the member values of the entries).
-	 */
-	//private Object[] refs;
-	
     public NewObject(INeo4emfObject value) {
         super(value);
-        //int refSize = value.eClass().getEStructuralFeatures().size();
-       /* refs = new Object[refSize];
-        Iterator<EStructuralFeature> it = value.eClass().getEStructuralFeatures().iterator();
-        int i = 0;
-        while(it.hasNext()) {
-        	refs[i] = value.eGet(it.next());
-        	i++;
-        }*/
     }
 
 	@Override
