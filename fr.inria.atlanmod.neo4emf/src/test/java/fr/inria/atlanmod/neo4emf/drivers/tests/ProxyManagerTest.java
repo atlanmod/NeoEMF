490b650a33ff70b4eb54de0fa6a8c3b50702ecc6
gdaniel <zelus444@gmail.com>
2014-03-20 17:12:23 +0100

    more tests on proxy

--- fr.inria.atlanmod.neo4emf/src/test/java/fr/inria/atlanmod/neo4emf/drivers/tests/ProxyManagerTest.java
+++ fr.inria.atlanmod.neo4emf/src/test/java/fr/inria/atlanmod/neo4emf/drivers/tests/ProxyManagerTest.java
@@ -1,12 +1,11 @@
 package fr.inria.atlanmod.neo4emf.drivers.tests;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,8 +19,10 @@ import fr.inria.atlanmod.neo4emf.INeo4emfResource;
 import fr.inria.atlanmod.neo4emf.drivers.IProxyManager;
 import fr.inria.atlanmod.neo4emf.drivers.NESession;
 import fr.inria.atlanmod.neo4emf.testdata.Container;
+import fr.inria.atlanmod.neo4emf.testdata.Link;
 import fr.inria.atlanmod.neo4emf.testdata.TestFactory;
 import fr.inria.atlanmod.neo4emf.testdata.TestPackage;
+import fr.inria.atlanmod.neo4emf.testdata.Vertex;
 
 public class ProxyManagerTest {
 	
@@ -65,5 +66,83 @@ public class ProxyManagerTest {
 		Cache<Long,INeo4emfObject> cache = proxy.getInternalProxy().get(c.eClass());
 		assert cache.getIfPresent(c.getNodeId()) != null : "The cached value for id " + c.getNodeId() + " is null";
 		assert cache.getIfPresent(c.getNodeId()) == c : "Wrong cached value for id " + c.getNodeId();
+	}
+	
+	@Test
+	public void testPutManyToProxy() {
+		int count = 1;
+		Container[] containers = new Container[count];
+		Vertex[] vertices = new Vertex[count];
+		Link[] links = new Link[count];
+		for(int i = 0; i < count; i++) {
+			containers[i] = factory.createContainer();
+			vertices[i] = factory.createVertex();
+			links[i] = factory.createLink();
+			containers[i].setNodeId(i);
+			vertices[i].setNodeId(i);
+			links[i].setNodeId(i);
+			proxy.putToProxy(containers[i]);
+			proxy.putToProxy(vertices[i]);
+			proxy.putToProxy(links[i]);
+		}
+		Map<EClass,Cache<Long,INeo4emfObject>> internalProxy = proxy.getInternalProxy();
+		Cache<Long,INeo4emfObject> containerCache = internalProxy.get(TestPackage.eINSTANCE.getContainer());
+		Cache<Long,INeo4emfObject> vertexCache = internalProxy.get(TestPackage.eINSTANCE.getVertex());
+		Cache<Long,INeo4emfObject> linkCache = internalProxy.get(TestPackage.eINSTANCE.getLink());
+		assert containerCache != null : "The Container EClass is not an entry of the proxy";
+		assert vertexCache != null : "The Vertex EClass is not an entry of the proxy";
+		assert linkCache != null : "The Link EClass is not an entry of the proxy";
+		for(int i = 0; i < count; i++) {
+			assert containerCache.getIfPresent((long)i) != null : "The cached Container value for id " + i + " is null";
+			assert vertexCache.getIfPresent((long)i) != null : "The cached Vertex value for id " + i + " is null";
+			assert linkCache.getIfPresent((long)i) != null : "The cached Link value for id " + i + " is null";
+			
+			assert containerCache.getIfPresent((long)i) == containers[i] : "Wrong object for id " + i + " in container cache";
+			assert vertexCache.getIfPresent((long)i) == vertices[i] : "Wrong object for id " + i + " in vertex cache";
+			assert linkCache.getIfPresent((long)i) == links[i] : "Wrong object for id " + i + " in link cache";
+		}
+	}
+	
+	/**
+	 * The idea is to do a clear that should not release the
+	 * object in the cache (there are some strong references 
+	 * on this object)
+	 */
+	@Test
+	public void testPutToProxyAndUselessClear() {
+		Container c = factory.createContainer();
+		proxy.putToProxy(c);
+		clearSoftReferences();
+		assert proxy.getInternalProxy().containsKey(c.eClass()) : "The EClass is not an entry of the proxy";
+		Cache<Long,INeo4emfObject> cache = proxy.getInternalProxy().get(c.eClass());
+		assert cache.getIfPresent(c.getNodeId()) != null : "The cached value for id " + c.getNodeId() + " is null";
+		assert cache.getIfPresent(c.getNodeId()) == c : "Wrong cached value for id " + c.getNodeId();
+	}
+	
+	/**
+	 * The idea is to do a clear that should release the 
+	 * object in the cache (there is no strong references
+	 * on this object)
+	 */
+	@Test
+	public void testPutToProxyAndClear() {
+		long id = 5;
+		Container c = factory.createContainer();
+		c.setNodeId(id);
+		proxy.putToProxy(c);
+		c = null;
+		clearSoftReferences();
+		// There is no way to remove the EClass from the proxy (because EClasses are not cached)
+		assert proxy.getInternalProxy().containsKey(TestPackage.eINSTANCE.getContainer()) : "The EClass is not an entry of the proxy";
+		Cache<Long,INeo4emfObject> cache = proxy.getInternalProxy().get(TestPackage.eINSTANCE.getContainer());
+		assert cache.getIfPresent(id) == null : "The entry hasn't been removed";
+	}
+	
+	private void clearSoftReferences() {
+		try {
+		    Object[] ignored = new Object[(int) Runtime.getRuntime().maxMemory()];
+		} catch (Throwable e) {
+		    // Ignore OME
+		}
 	}
 }
