6e69119a01a59019384d89840f83fd9dcf87db97
abelgomez <abel.gomez-llana@inria.fr>
2014-05-19 18:12:58 +0200

    Added license and copyright information

--- fr.inria.atlanmod.kyanos.ui/src/fr/inria/atlanmod/kyanos/ui/commands/MigrateCommand.java
+++ fr.inria.atlanmod.kyanos.ui/src/fr/inria/atlanmod/kyanos/ui/commands/MigrateCommand.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2013 Atlanmod INRIA LINA Mines Nantes
+ * Copyright (c) 2014 Abel Gómez.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
- *******************************************************************************/
+ *     Abel Gómez - initial API and implementation
+ ******************************************************************************/
 package fr.inria.atlanmod.kyanos.ui.commands;
 
 import java.util.Map;
