970fc7fcf339f0029ac874edeeddc464d251982b
abelgomez <abel.gomez-llana@inria.fr>
2014-05-19 18:04:37 +0200

    Added license and copyright information

--- fr.inria.atlanmod.kyanos/src/fr/inria/atlanmod/kyanos/datastore/estores/impl/AutocommitGraphResourceEStoreImpl.java
+++ fr.inria.atlanmod.kyanos/src/fr/inria/atlanmod/kyanos/datastore/estores/impl/AutocommitGraphResourceEStoreImpl.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Abel Gómez.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Abel Gómez - initial API and implementation
+ ******************************************************************************/
 package fr.inria.atlanmod.kyanos.datastore.estores.impl;
 
 import org.eclipse.emf.ecore.EStructuralFeature;
--- fr.inria.atlanmod.kyanos/src/fr/inria/atlanmod/kyanos/logger/Logger.java
+++ fr.inria.atlanmod.kyanos/src/fr/inria/atlanmod/kyanos/logger/Logger.java
@@ -1,17 +1,14 @@
-package fr.inria.atlanmod.kyanos.logger;
-
-/**
- * Copyright (c) 2013 Atlanmod INRIA LINA Mines Nantes
+/*******************************************************************************
+ * Copyright (c) 2014 Abel Gómez.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *    Atlanmod INRIA LINA Mines Nantes - initial API and implementation
- * Descritpion ! To come
- * @author Amine BENELALLAM
- * */
+ *     Abel Gómez - initial API and implementation
+ ******************************************************************************/
+package fr.inria.atlanmod.kyanos.logger;
 
 import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IStatus;
