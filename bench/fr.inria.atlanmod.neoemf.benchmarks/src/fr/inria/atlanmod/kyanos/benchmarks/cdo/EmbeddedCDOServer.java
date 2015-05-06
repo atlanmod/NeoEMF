9b64de10be6028faa9c4ea6e5a1faa613a0048f3
gwendal <gwendal.daniel@gmail.com>
2015-05-06 15:02:28 +0200

    change copyright headers in bench

--- bench/fr.inria.atlanmod.neoemf.benchmarks/src/fr/inria/atlanmod/kyanos/benchmarks/cdo/EmbeddedCDOServer.java
+++ bench/fr.inria.atlanmod.neoemf.benchmarks/src/fr/inria/atlanmod/kyanos/benchmarks/cdo/EmbeddedCDOServer.java
@@ -1,15 +1,4 @@
 /*******************************************************************************
- * Copyright (c) 2014 Abel Gómez.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     Abel Gómez - initial API and implementation
- ******************************************************************************/
-package fr.inria.atlanmod.kyanos.benchmarks.cdo;
-/*******************************************************************************
  * Copyright (c) 2010 Stephan Zehrer and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -19,7 +8,8 @@ package fr.inria.atlanmod.kyanos.benchmarks.cdo;
  * Contributors:
  *    Stephan Zehrer - initial API and implementation
  *******************************************************************************/
- 
+package fr.inria.atlanmod.kyanos.benchmarks.cdo;
+
 import java.util.HashMap;
 import java.util.Map;
 
