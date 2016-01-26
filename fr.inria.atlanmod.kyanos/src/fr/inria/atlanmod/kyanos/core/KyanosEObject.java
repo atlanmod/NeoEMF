1627f2a194b150394ea50a4875c99a81b7a526fc
Amine Benelallam <amine.benallam@gmail.com>
2016-01-26 15:26:05 +0100

    Change classpath and build files

--- fr.inria.atlanmod.kyanos/src/fr/inria/atlanmod/kyanos/core/KyanosEObject.java
+++ fr.inria.atlanmod.kyanos/src/fr/inria/atlanmod/kyanos/core/KyanosEObject.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2014 Abel Gómez.
+ * Copyright (c) 2014 Abel Gomez.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     Abel Gómez - initial API and implementation
+ *     Abel Gï¿½mez - initial API and implementation
  ******************************************************************************/
 package fr.inria.atlanmod.kyanos.core;
 
--- fr.inria.atlanmod.kyanos/src/fr/inria/atlanmod/kyanos/core/KyanosResource.java
+++ fr.inria.atlanmod.kyanos/src/fr/inria/atlanmod/kyanos/core/KyanosResource.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2014 Abel Gómez.
+ * Copyright (c) 2014 Abel Gï¿½mez.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     Abel Gómez - initial API and implementation
+ *     Abel Gï¿½mez - initial API and implementation
  ******************************************************************************/
 package fr.inria.atlanmod.kyanos.core;
 
@@ -17,7 +17,12 @@ public interface KyanosResource extends Resource, Resource.Internal {
 
 	public final static String OPTIONS_GRAPH_TYPE = "blueprints.graph";
 	public final static String OPTIONS_GRAPH_TYPE_DEFAULT = "com.tinkerpop.blueprints.impls.tg.TinkerGraph";
-
+	/**
+	 * an option for creating read-only models
+	 * the value by default is {@value <code>false</code>}
+	 */
+	String  OPTIONS_HBASE_READ_ONLY = "hbase.readOnly";
+	boolean OPTIONS_HBASE_READ_ONLY_DEFAULT = false; 
 	
 	public abstract InternalEObject.EStore eStore();
 	
