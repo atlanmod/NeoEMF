954a805f32ee897a9129b20c2c04baaba3f9a8de
Yoann <y.vernageau@gmail.com>
2016-05-05 17:47:25 +0200

    Fix accidentally added license.

--- core/src/main/java/fr/inria/atlanmod/neoemf/datastore/estores/impl/OwnedTransientEStoreImpl.java
+++ core/src/main/java/fr/inria/atlanmod/neoemf/datastore/estores/impl/OwnedTransientEStoreImpl.java
@@ -4,10 +4,11 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * <p>
+ *
  * Contributors:
- * Atlanmod INRIA LINA Mines Nantes - initial API and implementation
+ *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
  *******************************************************************************/
+
 package fr.inria.atlanmod.neoemf.datastore.estores.impl;
 
 import org.eclipse.emf.ecore.EClass;
--- tests/src/test/java/fr/inria/atlanmod/neoemf/tests/AllSavedLoadedResourceTest.java
+++ tests/src/test/java/fr/inria/atlanmod/neoemf/tests/AllSavedLoadedResourceTest.java
@@ -4,10 +4,11 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * <p>
+ *
  * Contributors:
- * Atlanmod INRIA LINA Mines Nantes - initial API and implementation
+ *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
  *******************************************************************************/
+
 package fr.inria.atlanmod.neoemf.tests;
 
 import fr.inria.atlanmod.neoemf.resources.PersistentResource;
