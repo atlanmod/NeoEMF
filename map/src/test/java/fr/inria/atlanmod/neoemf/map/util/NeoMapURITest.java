54c536c5e416bffd8e93d37ec5ae8300cdeb36e5
Yoann <y.vernageau@gmail.com>
2016-05-25 13:08:22 +0200

    update the copyright license as recommended Checkstyle

--- map/src/test/java/fr/inria/atlanmod/neoemf/map/util/NeoMapURITest.java
+++ map/src/test/java/fr/inria/atlanmod/neoemf/map/util/NeoMapURITest.java
@@ -1,5 +1,5 @@
-/*******************************************************************************
- * Copyright (c) 2013 Atlanmod INRIA LINA Mines Nantes
+/*
+ * Copyright (c) 2013 Atlanmod INRIA LINA Mines Nantes.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
- *******************************************************************************/
+ */
 
 package fr.inria.atlanmod.neoemf.map.util;
 
