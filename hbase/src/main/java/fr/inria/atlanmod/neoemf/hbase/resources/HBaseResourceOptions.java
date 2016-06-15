484d4ea3ede252f62cc0f135e59265325f7cd026
gwendal <gwendal.daniel@gmail.com>
2016-06-15 12:55:24 +0200

    add first version of HBase implementation

---
+++ hbase/src/main/java/fr/inria/atlanmod/neoemf/hbase/resources/HBaseResourceOptions.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Atlanmod INRIA LINA Mines Nantes
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
+ *******************************************************************************/
+package fr.inria.atlanmod.neoemf.hbase.resources;
+
+import fr.inria.atlanmod.neoemf.resources.PersistentResourceOptions;
+
+public interface HBaseResourceOptions extends PersistentResourceOptions {
+    
+    public final static String OPTIONS_HBASE_READ_ONLY = "hbase.readOnly";
+
+}
