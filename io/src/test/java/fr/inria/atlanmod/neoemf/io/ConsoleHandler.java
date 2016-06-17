e5185d70d4f44b1f63b25d45ccecc126dac12802
Yoann <y.vernageau@gmail.com>
2016-06-17 13:24:17 +0200

    add basic tests to launch the IO module

---
+++ io/src/test/java/fr/inria/atlanmod/neoemf/io/ConsoleHandler.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2013 Atlanmod INRIA LINA Mines Nantes.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
+ */
+
+package fr.inria.atlanmod.neoemf.io;
+
+/**
+ *
+ */
+public class ConsoleHandler implements Handler {
+
+    private String name;
+
+    public ConsoleHandler(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void handleStartDocument() {
+        //System.out.println("[" + name +  "] Start the reading of a document");
+    }
+
+    @Override
+    public void handleStartElement(String namespace, String localName) {
+        System.out.println("[" + name +  "] Element : " + namespace + ":" + localName);
+    }
+
+    @Override
+    public void handleAttribute(String namespace, String localName, String value) {
+        System.out.println("[" + name +  "]  - Attribute : " + namespace + ":" + localName + " = " + value);
+    }
+
+    @Override
+    public void handleReference(String namespace, String localName, String reference) {
+        System.out.println("[" + name +  "]  - Reference : " + namespace + ":" + localName + " = " + reference);
+    }
+
+    @Override
+    public void handleEndElement() {
+        //System.out.println("[" + name +  "] End of element");
+    }
+
+    @Override
+    public void handleEndDocument() {
+        //System.out.println("[" + name +  "] Document reading done");
+    }
+}
---
+++ io/src/test/java/fr/inria/atlanmod/neoemf/io/XmiReaderTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2013 Atlanmod INRIA LINA Mines Nantes.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
+ */
+
+package fr.inria.atlanmod.neoemf.io;
+
+import fr.inria.atlanmod.neoemf.AllTest;
+import fr.inria.atlanmod.neoemf.io.impl.AbstractInternalHandler;
+import fr.inria.atlanmod.neoemf.io.impl.InternalXmlHandler;
+import fr.inria.atlanmod.neoemf.io.reader.Reader;
+import fr.inria.atlanmod.neoemf.io.reader.XmiStreamReader;
+import fr.inria.atlanmod.neoemf.logger.NeoLogger;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.fail;
+
+/**
+ *
+ */
+public class XmiReaderTest extends AllTest {
+
+    private static final String THIN_XMI = "/fr.inria.atlanmod.kyanos.tests.xmi";
+    private static final String LIGHT_XMI = "/fr.inria.atlanmod.neo4emf.neo4jresolver.xmi";
+    private static final String MEDIUM_XMI = "/org.eclipse.gmt.modisco.java.kyanos.xmi";
+    private static final String HEAVY_XMI = "/org.eclipse.jdt.core.xmi";
+    private static final String MONSTER_XMI = "/org.eclipse.jdt.source.all.xmi";
+
+    private Reader reader;
+
+    @Before
+    public void setUp() throws Exception {
+        AbstractInternalHandler internalHandler = new InternalXmlHandler();
+        internalHandler.addHandler(new ConsoleHandler("console1"));
+
+        reader = new XmiStreamReader();
+        reader.addHandler(internalHandler);
+    }
+
+    @Test
+    public void readThin() throws Exception {
+        File xmi = new File(XmiReaderTest.class.getResource(THIN_XMI).getFile());
+        read(xmi);
+    }
+
+    @Test
+    public void readLight() throws Exception {
+        File xmi = new File(XmiReaderTest.class.getResource(LIGHT_XMI).getFile());
+        read(xmi);
+    }
+
+    @Test
+    public void readMedium() throws Exception {
+        File xmi = new File(XmiReaderTest.class.getResource(MEDIUM_XMI).getFile());
+        read(xmi);
+    }
+
+    @Test
+    @Ignore
+    public void readHeavy() throws Exception {
+        File xmi = new File(XmiReaderTest.class.getResource(HEAVY_XMI).getFile());
+        read(xmi);
+    }
+
+    @Test
+    @Ignore
+    public void readMonster() throws Exception {
+        File xmi = new File(XmiReaderTest.class.getResource(MONSTER_XMI).getFile());
+        read(xmi);
+    }
+
+    public void read(File file) throws Exception {
+        try {
+            reader.read(file);
+        } catch (Exception e) {
+            NeoLogger.error(e);
+            fail(e.getMessage());
+        }
+    }
+}
