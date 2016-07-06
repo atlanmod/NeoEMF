55b09c1778174e95d07f2c23f4659c44bdfd32d3
Yoann <y.vernageau@gmail.com>
2016-07-06 12:56:01 +0200

    add structural tests in integration tests for import

--- tests/src/test/java/fr/inria/atlanmod/neoemf/tests/io/ImportTest.java
+++ tests/src/test/java/fr/inria/atlanmod/neoemf/tests/io/ImportTest.java
@@ -25,6 +25,7 @@ import fr.inria.atlanmod.neoemf.io.PersistenceHandler;
 import fr.inria.atlanmod.neoemf.resources.PersistentResourceFactory;
 import fr.inria.atlanmod.neoemf.resources.PersistentResourceOptions;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
@@ -52,8 +53,10 @@ import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.fail;
 
 /**
  *
@@ -78,13 +81,129 @@ public class ImportTest extends AllInputTest {
     }
 
     @Test
-    public void testImportWithoutIdNeo4j() throws Exception {
+    public void testCompareWithEMFNeo4j() throws Exception {
         File file = getXmiStandard();
 
         EObject emfObject = readWithEMF(new FileInputStream(file));
         EObject neoObject = readWithNeo(new FileInputStream(file));
 
         assertEqualEObject(neoObject, emfObject);
+    }
+
+    /**
+     * Check that the elements are properly processed.
+     * <p/>
+     * All elements must have an id and a class name.
+     */
+    @Test
+    public void testElementsAndChildrenNeo4j() throws Exception {
+        EObject eObject;
+        EObject eObjectChild;
+
+        EObject root = readWithNeo(new FileInputStream(getXmiStandard()));
+        assertValidElement(root, "Model", 19, "fr.inria.atlanmod.kyanos.tests");
+        {
+            //@Model/@ownedElements.0/@ownedPackages[4]/@ownedElements.0
+            eObject = getChildFrom(root, 0, 0, 0, 0, 0, 0);
+            assertValidElement(eObject, "ClassDeclaration", 7, "TestCreateResource");
+            {
+                //@Model/@ownedElements.0/@ownedPackages[4]/@ownedElements.0/@modifier
+                eObjectChild = getChildFrom(eObject, 0);
+                assertValidElement(eObjectChild, "Modifier", 0, null);
+
+                //@Model/@ownedElements.0/@ownedPackages[4]/@ownedElements.0/@bodyDeclarations.2
+                eObjectChild = getChildFrom(eObject, 3);
+                assertValidElement(eObjectChild, "MethodDeclaration", 5, "tearDownAfterClass");
+            }
+
+            //@Model/@ownedElements.1
+            eObject = getChildFrom(root, 1);
+            assertValidElement(eObject, "Package", 5, "java");
+
+            //@Model/@orphanTypes.5
+            eObject = getChildFrom(root, 8);
+            assertValidElement(eObject, "PrimitiveTypeVoid", 0, "void");
+
+            //@Model/@compilationUnits.1
+            eObject = getChildFrom(root, 17);
+            assertValidElement(eObject, "CompilationUnit", 16, "TestXmi.java");
+            {
+                //@Model/@compilationUnits.1/@imports.2
+                eObjectChild = getChildFrom(eObject, 2);
+                assertValidElement(eObjectChild, "ImportDeclaration", 0, null);
+            }
+        }
+    }
+
+    /**
+     * Check that the attributes are properly processed.
+     */
+    @Test
+    public void testAttributes() throws Exception {
+        EObject eObject;
+
+        EObject root = readWithNeo(new FileInputStream(getXmiStandard()));
+        {
+            //@Model/@ownedElements.0/@ownedPackages[4]/@ownedElements.0/@modifier
+            eObject = getChildFrom(root, 0, 0, 0, 0, 0, 0, 0);
+            assertValidAttribute(eObject, "visibility", "public");
+
+            //@Model/@ownedElements.1
+            eObject = getChildFrom(root, 1);
+            assertValidAttribute(eObject, "proxy", "true");
+
+            //@Model/@compilationUnits.1
+            eObject = getChildFrom(root, 17);
+            assertValidAttribute(eObject, "originalFilePath", "C:\\Eclipse\\eclipse-SDK-4.3.1-win32-x86_64-Blue\\eclipse\\workspace\\fr.inria.atlanmod.kyanos.tests\\src\\fr\\inria\\atlanmod\\kyanos\\tests\\TestXmi.java");
+        }
+    }
+
+    /**
+     * Check that the {@code XPath} references/id are properly processed.
+     * <p/>
+     * Containment and inverse reference must have been created.
+     * References previously detected as attributes, are now well placed.
+     */
+    @Test
+    public void testReferences() throws Exception {
+        EObject eObject;
+        EObject eObjectChild;
+
+        EObject root = readWithNeo(new FileInputStream(getXmiStandard()));
+        assertValidReference(root, "ownedElements", 0, "Package", "fr", true, true);
+        assertValidReference(root, "orphanTypes", 5, "PrimitiveTypeVoid", "void", true, true);
+        {
+            //@Model/@ownedElements.0/@ownedPackages[4]/@ownedElements.0
+            eObject = getChildFrom(root, 0, 0, 0, 0, 0, 0);
+            assertValidReference(eObject, "originalCompilationUnit", 0, "CompilationUnit", "TestCreateResource.java", false, false);
+            assertValidReference(eObject, "bodyDeclarations", 0, "FieldDeclaration", null, true, true);
+            {
+                //@Model/@ownedElements.0/@ownedPackages[4]/@ownedElements.0/@bodyDeclarations.2
+                eObjectChild = getChildFrom(eObject, 3);
+                assertValidReference(eObjectChild, "originalCompilationUnit", 0, "CompilationUnit", "TestCreateResource.java", false, false);
+                assertValidReference(eObjectChild, "modifier", 0, "Modifier", null, false, true);
+            }
+
+            //@Model/@ownedElements.1
+            eObject = getChildFrom(root, 1);
+            assertValidReference(eObject, "ownedPackages", 0, "Package", "io", true, true);
+
+            //@Model/@orphanTypes.5
+            eObject = getChildFrom(root, 8);
+            assertValidReference(eObject, "usagesInTypeAccess", 0, "TypeAccess", null, true, false);
+            assertValidReference(eObject, "usagesInTypeAccess", 9, "TypeAccess", null, true, false);
+
+            //@Model/@compilationUnits.1
+            eObject = getChildFrom(root, 17);
+            assertValidReference(eObject, "package", 0, "Package", "tests", false, false);
+            assertValidReference(eObject, "imports", 0, "ImportDeclaration", null, true, true);
+            {
+                //@Model/@compilationUnits.1/@imports.2
+                eObjectChild = getChildFrom(eObject, 2);
+                assertValidReference(eObjectChild, "originalCompilationUnit", 0, "CompilationUnit", "TestXmi.java", false, false);
+                assertValidReference(eObjectChild, "importedElement", 2, "ClassDeclaration", "URI", false, false);
+            }
+        }
     }
 
     @Test
@@ -206,5 +325,76 @@ public class ImportTest extends AllInputTest {
                 assertThat(actualValue, equalTo(expectedValue));
             }
         }
+    }
+
+    private void assertValidElement(final EObject eObject, final String className, final int size, final Object name) {
+        assertThat(eObject.eClass().getName(), equalTo(className));
+        assertThat(eObject.eContents(), hasSize(size));
+        if (name != null) {
+            assertThat(eObject.eGet(eObject.eClass().getEStructuralFeature("name")), equalTo(name));
+        } else {
+            try {
+                eObject.eGet(eObject.eClass().getEStructuralFeature("name"));
+                fail();
+            } catch (NullPointerException e) {
+                // It's good !
+            }
+        }
+    }
+
+    private void assertValidReference(final EObject eObject, final String name, final int index, final String referenceClassName, final String referenceName, final boolean many, final boolean containment) {
+        EReference eReference = (EReference) eObject.eClass().getEStructuralFeature(name);
+
+        Object objectReference = eObject.eGet(eReference);
+        EObject eObjectReference;
+
+        if (many) {
+            @SuppressWarnings("unchecked")
+            EList<EObject> eObjectList = (EList<EObject>) objectReference;
+            eObjectReference = eObjectList.get(index);
+        } else {
+            eObjectReference = (EObject) objectReference;
+        }
+
+        assertThat(eObjectReference.eClass().getName(), equalTo(referenceClassName));
+
+        if (referenceName != null) {
+            EAttribute eAttribute = (EAttribute) eObjectReference.eClass().getEStructuralFeature("name");
+            assertThat(eObjectReference.eGet(eAttribute).toString(), equalTo(referenceName));
+        } else {
+            try {
+                EAttribute eAttribute = (EAttribute) eObjectReference.eClass().getEStructuralFeature("name");
+                assertThat(eObjectReference.eGet(eAttribute), equalTo(eAttribute.getDefaultValue()));
+            } catch (NullPointerException e) {
+                // It's good
+            }
+        }
+
+        assertThat(eReference.isContainment(), is(containment));
+        assertThat(eReference.isMany(), is(many));
+    }
+
+    private void assertValidAttribute(final EObject eObject, final String name, final Object value) {
+        EAttribute eAttribute = (EAttribute) eObject.eClass().getEStructuralFeature(name);
+
+        if (value == null) {
+            assertThat(eObject.eGet(eAttribute), equalTo(eAttribute.getDefaultValue()));
+        } else {
+            assertThat(eObject.eGet(eAttribute).toString(), equalTo(value));
+        }
+    }
+
+    private static EObject getChildFrom(EObject root, int ... indexes) {
+        if (indexes.length == 0) {
+            throw new IllegalArgumentException("You must define at least one index");
+        }
+
+        EObject child = root;
+
+        for (int index : indexes) {
+            child = child.eContents().get(index);
+        }
+
+        return child;
     }
 }
