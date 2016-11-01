49732c8aa0bcdf7eea93d094612ebcbe05af4a26
Yoann <y.vernageau@gmail.com>
2016-11-01 10:44:42 +0100

    add a Maven version of benchmarks

---
+++ benchmarks/core/src/main/java/fr/inria/atlanmod/neoemf/benchmarks/CdoQueryRenameAllMethods.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2013-2016 Atlanmod INRIA LINA Mines Nantes.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
+ */
+
+package fr.inria.atlanmod.neoemf.benchmarks;
+
+import fr.inria.atlanmod.neoemf.benchmarks.cdo.EmbeddedCDOServer;
+import fr.inria.atlanmod.neoemf.benchmarks.queries.JavaQueries;
+import fr.inria.atlanmod.neoemf.benchmarks.util.MessageUtil;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.eclipse.emf.cdo.net4j.CDONet4jSession;
+import org.eclipse.emf.cdo.session.CDOSession;
+import org.eclipse.emf.cdo.transaction.CDOTransaction;
+import org.eclipse.emf.ecore.resource.Resource;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CdoQueryRenameAllMethods {
+
+    private static final Logger LOG = Logger.getLogger(CdoQueryRenameAllMethods.class.getName());
+
+    private static final String IN = "input";
+
+    private static final String REPO_NAME = "reponame";
+
+    private static final String EPACKAGE_CLASS = "epackage_class";
+
+    public static void main(String[] args) {
+        Options options = new Options();
+
+        Option inputOpt = OptionBuilder.create(IN);
+        inputOpt.setArgName("INPUT");
+        inputOpt.setDescription("Input CDO resource directory");
+        inputOpt.setArgs(1);
+        inputOpt.setRequired(true);
+
+        Option inClassOpt = OptionBuilder.create(EPACKAGE_CLASS);
+        inClassOpt.setArgName("CLASS");
+        inClassOpt.setDescription("FQN of EPackage implementation class");
+        inClassOpt.setArgs(1);
+        inClassOpt.setRequired(true);
+
+        Option repoOpt = OptionBuilder.create(REPO_NAME);
+        repoOpt.setArgName("REPO_NAME");
+        repoOpt.setDescription("CDO Repository name");
+        repoOpt.setArgs(1);
+        repoOpt.setRequired(true);
+
+        options.addOption(inputOpt);
+        options.addOption(inClassOpt);
+        options.addOption(repoOpt);
+
+        CommandLineParser parser = new PosixParser();
+
+        try {
+            CommandLine commandLine = parser.parse(options, args);
+
+            String repositoryDir = commandLine.getOptionValue(IN);
+            String repositoryName = commandLine.getOptionValue(REPO_NAME);
+
+            Class<?> inClazz = CdoQueryRenameAllMethods.class.getClassLoader().loadClass(commandLine.getOptionValue(EPACKAGE_CLASS));
+            inClazz.getMethod("init").invoke(null);
+
+            EmbeddedCDOServer server = new EmbeddedCDOServer(repositoryDir, repositoryName);
+            try {
+                server.run();
+                CDOSession session = server.openSession();
+                ((CDONet4jSession) session).options().setCommitTimeout(50 * 1000);
+                CDOTransaction transaction = session.openTransaction();
+                Resource resource = transaction.getRootResource();
+
+                String name = UUID.randomUUID().toString();
+                {
+                    LOG.log(Level.INFO, "Start query");
+                    long begin = System.currentTimeMillis();
+                    JavaQueries.renameAllMethods(resource, name);
+                    long end = System.currentTimeMillis();
+                    transaction.commit();
+                    LOG.log(Level.INFO, "End query");
+                    LOG.log(Level.INFO, MessageFormat.format("Time spent: {0}", MessageUtil.formatMillis(end - begin)));
+                }
+
+//				{
+//					transaction.close();
+//					session.close();
+//					
+//					session = server.openSession();
+//					transaction = session.openTransaction();
+//					resource = transaction.getRootResource();
+//					
+//					EList<? extends EObject> methodList = JavaQueries.getAllInstances(resource, JavaPackage.eINSTANCE.getMethodDeclaration());
+//					int i = 0;
+//					for (EObject eObject: methodList) {
+//						MethodDeclaration method = (MethodDeclaration) eObject;
+//						if (name.equals(method.getName())) {
+//							i++;
+//						}
+//					}
+//					LOG.log(Level.INFO, MessageFormat.format("Renamed {0} methods", i));
+//				}
+
+                transaction.close();
+                session.close();
+            }
+            finally {
+                server.stop();
+            }
+        }
+        catch (ParseException e) {
+            MessageUtil.showError(e.toString());
+            MessageUtil.showError("Current arguments: " + Arrays.toString(args));
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("java -jar <this-file.jar>", options, true);
+        }
+        catch (Throwable e) {
+            MessageUtil.showError(e.toString());
+        }
+    }
+}
---
+++ benchmarks/core/src/main/java/fr/inria/atlanmod/neoemf/benchmarks/ase2015/CdoQueryGrabats09.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2013-2016 Atlanmod INRIA LINA Mines Nantes.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
+ */
+
+package fr.inria.atlanmod.neoemf.benchmarks.ase2015;
+
+import fr.inria.atlanmod.neoemf.benchmarks.ase2015.queries.ASE2015JavaQueries;
+import fr.inria.atlanmod.neoemf.benchmarks.cdo.EmbeddedCDOServer;
+import fr.inria.atlanmod.neoemf.benchmarks.util.MessageUtil;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.eclipse.emf.cdo.session.CDOSession;
+import org.eclipse.emf.cdo.transaction.CDOTransaction;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.gmt.modisco.java.ClassDeclaration;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CdoQueryGrabats09 {
+
+    private static final Logger LOG = Logger.getLogger(CdoQueryGrabats09.class.getName());
+
+    private static final String IN = "input";
+
+    private static final String REPO_NAME = "reponame";
+
+    private static final String EPACKAGE_CLASS = "epackage_class";
+
+    public static void main(String[] args) {
+        Options options = new Options();
+
+        Option inputOpt = OptionBuilder.create(IN);
+        inputOpt.setArgName("INPUT");
+        inputOpt.setDescription("Input CDO resource directory");
+        inputOpt.setArgs(1);
+        inputOpt.setRequired(true);
+
+        Option inClassOpt = OptionBuilder.create(EPACKAGE_CLASS);
+        inClassOpt.setArgName("CLASS");
+        inClassOpt.setDescription("FQN of EPackage implementation class");
+        inClassOpt.setArgs(1);
+        inClassOpt.setRequired(true);
+
+        Option repoOpt = OptionBuilder.create(REPO_NAME);
+        repoOpt.setArgName("REPO_NAME");
+        repoOpt.setDescription("CDO Repository name");
+        repoOpt.setArgs(1);
+        repoOpt.setRequired(true);
+
+        options.addOption(inputOpt);
+        options.addOption(inClassOpt);
+        options.addOption(repoOpt);
+
+        CommandLineParser parser = new PosixParser();
+
+        try {
+            CommandLine commandLine = parser.parse(options, args);
+
+            String repositoryDir = commandLine.getOptionValue(IN);
+            String repositoryName = commandLine.getOptionValue(REPO_NAME);
+
+            Class<?> inClazz = CdoQueryGrabats09.class.getClassLoader().loadClass(commandLine.getOptionValue(EPACKAGE_CLASS));
+            inClazz.getMethod("init").invoke(null);
+
+            EmbeddedCDOServer server = new EmbeddedCDOServer(repositoryDir, repositoryName);
+            try {
+                server.run();
+                CDOSession session = server.openSession();
+                CDOTransaction transaction = session.openTransaction();
+                Resource resource = transaction.getRootResource();
+
+                {
+                    Runtime.getRuntime().gc();
+                    long initialUsedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+                    LOG.log(Level.INFO, MessageFormat.format("Used memory before query: {0}", MessageUtil.byteCountToDisplaySize(initialUsedMemory)));
+                    LOG.log(Level.INFO, "Start query");
+                    long begin = System.currentTimeMillis();
+                    EList<ClassDeclaration> list = ASE2015JavaQueries.grabats09(resource);
+                    long end = System.currentTimeMillis();
+                    LOG.log(Level.INFO, "End query");
+                    LOG.log(Level.INFO, MessageFormat.format("Query result contains {0} elements", list.size()));
+                    LOG.log(Level.INFO, MessageFormat.format("Time spent: {0}", MessageUtil.formatMillis(end - begin)));
+                    Runtime.getRuntime().gc();
+                    long finalUsedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+                    LOG.log(Level.INFO, MessageFormat.format("Used memory after query: {0}", MessageUtil.byteCountToDisplaySize(finalUsedMemory)));
+                    LOG.log(Level.INFO, MessageFormat.format("Memory use increase: {0}", MessageUtil.byteCountToDisplaySize(finalUsedMemory - initialUsedMemory)));
+                }
+
+                transaction.close();
+                session.close();
+            }
+            finally {
+                server.stop();
+            }
+        }
+        catch (ParseException e) {
+            MessageUtil.showError(e.toString());
+            MessageUtil.showError("Current arguments: " + Arrays.toString(args));
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("java -jar <this-file.jar>", options, true);
+        }
+        catch (Throwable e) {
+            MessageUtil.showError(e.toString());
+        }
+    }
+}
