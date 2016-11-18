2c3962b9a444dd36fdc5fec14d91e2811b3c224a
Yoann <y.vernageau@gmail.com>
2016-11-18 01:35:19 +0100

    add a benchmark to compare the creation of datastores

--- benchmarks/core/src/main/java/fr/inria/atlanmod/neoemf/benchmarks/runner/state/ReadOnlyRunnerState.java
+++ benchmarks/core/src/main/java/fr/inria/atlanmod/neoemf/benchmarks/runner/state/ReadOnlyRunnerState.java
@@ -1,11 +1,60 @@
 package fr.inria.atlanmod.neoemf.benchmarks.runner.state;
 
+import fr.inria.atlanmod.neoemf.benchmarks.datastore.Backend;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.TearDown;
+
 import java.io.File;
+import java.util.Objects;
 
-public class ReadOnlyRunnerState extends AbstractRunnerState {
+/**
+ * This state provided a ready-to-use datastore. It is automatically preloaded and unloaded from the default location.
+ */
+public class ReadOnlyRunnerState extends RunnerState {
 
-    @Override
-    protected File getResourceFile() {
-        return resourceFile;
+    protected Resource resource;
+
+    protected File storeFile;
+
+    /**
+     * Returns the resource loaded from the datastore.
+     */
+    public Resource getResource() {
+        if (Objects.isNull(resource)) {
+            throw new NullPointerException();
+        }
+        return resource;
+    }
+
+    /**
+     * Returns the location of the datastore.
+     */
+    protected File getStoreLocation() {
+        return storeFile;
+    }
+
+    @Setup(Level.Trial)
+    public void initResource() throws Exception {
+        log.info("Initializing the datastore");
+        storeFile = getBackend().createStore(getResourceFile());
+    }
+
+    @Setup(Level.Invocation)
+    public void loadResource() throws Exception {
+        log.info("Loading the resource");
+        resource = getBackend().load(getStoreLocation());
+    }
+
+    @TearDown(Level.Invocation)
+    public void unloadResource() throws Exception {
+        log.info("Unloading the resource");
+        if (!Objects.isNull(resource)) {
+            getBackend().unload(resource);
+            resource = null;
+        }
+        Backend.clean();
     }
 }
