b43f657e85c01169fe15283f9862cb151506174e
Yoann <y.vernageau@gmail.com>
2016-11-19 15:15:44 +0100

    NeoLogger become asynchronous

--- core/src/main/java/fr/inria/atlanmod/neoemf/logging/NeoLogger.java
+++ core/src/main/java/fr/inria/atlanmod/neoemf/logging/NeoLogger.java
@@ -11,68 +11,67 @@
 
 package fr.inria.atlanmod.neoemf.logging;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.text.MessageFormat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NeoLogger {
 
     private static final Logger log = LogManager.getRootLogger();
 
+    private static final ExecutorService pool = Executors.newSingleThreadExecutor();
+
     private NeoLogger() {
     }
 
-    public static void debug(String msg) {
-        internalLog(Level.DEBUG, msg, null);
+    public static void debug(final String msg) {
+        pool.submit(() -> log.debug(msg));
     }
 
     public static void debug(String pattern, Object... args) {
-        internalLog(Level.DEBUG, MessageFormat.format(pattern, args), null);
+        pool.submit(() -> log.debug(MessageFormat.format(pattern, args)));
     }
 
     public static void info(String msg) {
-        internalLog(Level.INFO, msg, null);
+        pool.submit(() -> log.info(msg));
     }
 
     public static void info(String pattern, Object... args) {
-        internalLog(Level.INFO, MessageFormat.format(pattern, args), null);
+        pool.submit(() -> log.info(MessageFormat.format(pattern, args)));
     }
 
     public static void warn(String msg) {
-        internalLog(Level.WARN, msg, null);
+        pool.submit(() -> log.warn(msg));
     }
 
     public static void warn(String pattern, Object... args) {
-        internalLog(Level.WARN, MessageFormat.format(pattern, args), null);
+        pool.submit(() -> log.warn(MessageFormat.format(pattern, args)));
     }
 
     public static void warn(Throwable e) {
-        internalLog(Level.WARN, null, e);
+        pool.submit(() -> log.warn(e));
     }
 
     public static void warn(Throwable e, String pattern, Object... args) {
-        internalLog(Level.WARN, MessageFormat.format(pattern, args), e);
+        pool.submit(() -> log.warn(MessageFormat.format(pattern, args), e));
     }
 
     public static void error(String msg) {
-        internalLog(Level.ERROR, msg, null);
+        pool.submit(() -> log.error(msg));
     }
 
     public static void error(String pattern, Object... args) {
-        internalLog(Level.ERROR, MessageFormat.format(pattern, args), null);
+        pool.submit(() -> log.error(MessageFormat.format(pattern, args)));
     }
 
     public static void error(Throwable e) {
-        internalLog(Level.ERROR, null, e);
+        pool.submit(() -> log.error(e));
     }
 
     public static void error(Throwable e, String pattern, Object... args) {
-        internalLog(Level.ERROR, MessageFormat.format(pattern, args), e);
-    }
-
-    private static void internalLog(Level level, String msg, Throwable e) {
-        log.log(level, msg, e);
+        pool.submit(() -> log.error(MessageFormat.format(pattern, args), e));
     }
 }
