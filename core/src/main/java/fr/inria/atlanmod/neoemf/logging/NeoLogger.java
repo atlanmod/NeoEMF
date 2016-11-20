b55e8592bfbe9fdb4d7093d7dca033f5df8f62b7
Yoann <y.vernageau@gmail.com>
2016-11-20 02:08:16 +0100

    fix the thread that would not stop in NeoLogger

--- core/src/main/java/fr/inria/atlanmod/neoemf/logging/NeoLogger.java
+++ core/src/main/java/fr/inria/atlanmod/neoemf/logging/NeoLogger.java
@@ -15,81 +15,59 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.text.MessageFormat;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.RejectedExecutionException;
 
 public class NeoLogger {
 
     private static final Logger log = LogManager.getRootLogger();
 
-    private static final ExecutorService pool = Executors.newSingleThreadExecutor();
-
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            public void run() {
-                pool.shutdown();
-            }
-        });
-    }
-
     private NeoLogger() {
     }
 
-    private static void async(Runnable runnable) {
-        try {
-            pool.submit(runnable);
-        }
-        catch (RejectedExecutionException e) {
-            runnable.run();
-        }
-    }
-
     public static void debug(String msg) {
-        async(() -> log.debug(msg));
+        log.debug(msg);
     }
 
     public static void debug(String pattern, Object... args) {
-        async(() -> log.debug(MessageFormat.format(pattern, args)));
+        log.debug(MessageFormat.format(pattern, args));
     }
 
     public static void info(String msg) {
-        async(() -> log.info(msg));
+        log.info(msg);
     }
 
     public static void info(String pattern, Object... args) {
-        async(() -> log.info(MessageFormat.format(pattern, args)));
+        log.info(MessageFormat.format(pattern, args));
     }
 
     public static void warn(String msg) {
-        async(() -> log.warn(msg));
+        log.warn(msg);
     }
 
     public static void warn(String pattern, Object... args) {
-        async(() -> log.warn(MessageFormat.format(pattern, args)));
+        log.warn(MessageFormat.format(pattern, args));
     }
 
     public static void warn(Throwable e) {
-        async(() -> log.warn(e));
+        log.warn(e);
     }
 
     public static void warn(Throwable e, String pattern, Object... args) {
-        async(() -> log.warn(MessageFormat.format(pattern, args), e));
+        log.warn(MessageFormat.format(pattern, args), e);
     }
 
     public static void error(String msg) {
-        async(() -> log.error(msg));
+        log.error(msg);
     }
 
     public static void error(String pattern, Object... args) {
-        async(() -> log.error(MessageFormat.format(pattern, args)));
+        log.error(MessageFormat.format(pattern, args));
     }
 
     public static void error(Throwable e) {
-        async(() -> log.error(e));
+        log.error(e);
     }
 
     public static void error(Throwable e, String pattern, Object... args) {
-        async(() -> log.error(MessageFormat.format(pattern, args), e));
+        log.error(MessageFormat.format(pattern, args), e);
     }
 }
