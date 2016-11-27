1056f118f5cca7969ae698bdfd829342f420730e
Yoann <y.vernageau@gmail.com>
2016-11-27 18:55:07 +0100

    fix the creation of a wrong backend when using OptionsBuilder

--- graph/blueprints/src/main/java/fr/inria/atlanmod/neoemf/graph/blueprints/option/BlueprintsOptionsBuilder.java
+++ graph/blueprints/src/main/java/fr/inria/atlanmod/neoemf/graph/blueprints/option/BlueprintsOptionsBuilder.java
@@ -2,10 +2,6 @@ package fr.inria.atlanmod.neoemf.graph.blueprints.option;
 
 public class BlueprintsOptionsBuilder extends AbstractBlueprintsOptionsBuilder<BlueprintsOptionsBuilder> {
 
-    protected BlueprintsOptionsBuilder() {
-        graph(BlueprintsResourceOptions.GRAPH_TYPE_DEFAULT);
-    }
-
     public static BlueprintsOptionsBuilder newBuilder() {
         return new BlueprintsOptionsBuilder();
     }
