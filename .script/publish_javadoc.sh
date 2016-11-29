e83274906fde49d6563fc2fb6906fb18069ee5a9
Yoann <y.vernageau@gmail.com>
2016-11-29 15:46:24 +0100

    add elif condition

--- .script/publish_javadoc.sh
+++ .script/publish_javadoc.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-set -ev
+
+set -e
 
 if [ "$TRAVIS_REPO_SLUG" = "atlanmod/NeoEMF" ] && [ "$TRAVIS_JDK_VERSION" = "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-  echo -e "Publishing javadoc...\n"
+    echo -e "Publishing javadoc...\n"
 
-  cp -R target/site/apidocs $HOME/.neoemf/apidocs
-  cd $HOME
+    cp -R target/site/apidocs $HOME/.neoemf/apidocs
+    cd $HOME
 
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} gh-pages > /dev/null
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} gh-pages > /dev/null
 
-  cd gh-pages
+    cd gh-pages
 
-  git rm -rf ./apidocs
-  cp -Rf $HOME/.neoemf/apidocs ./apidocs
+     git rm -rf ./apidocs
+    cp -Rf $HOME/.neoemf/apidocs ./apidocs
 
-  git add -f .
-  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+    git add -f .
+    git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+     git push -fq origin gh-pages > /dev/null
 
-  echo -e "Published Javadoc to gh-pages.\n"
+    echo -e "Published Javadoc to gh-pages.\n"
+else
+    echo -e "No need to publish Javadoc\n";
 fi
