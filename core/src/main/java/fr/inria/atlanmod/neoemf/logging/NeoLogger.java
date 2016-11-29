4342424b11e3e454d468838f091e371a6d56fe5c
Yoann <y.vernageau@gmail.com>
2016-11-29 02:37:36 +0100

    make NeoLogger asynchronous

--- core/src/main/java/fr/inria/atlanmod/neoemf/logging/NeoLogger.java
+++ core/src/main/java/fr/inria/atlanmod/neoemf/logging/NeoLogger.java
@@ -11,63 +11,81 @@
 
 package fr.inria.atlanmod.neoemf.logging;
 
+import com.google.common.util.concurrent.MoreExecutors;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.text.MessageFormat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
 
-public class NeoLogger {
+public final class NeoLogger {
 
     private static final Logger log = LogManager.getRootLogger();
 
-    private NeoLogger() {
+    private static final ExecutorService pool = Executors.newSingleThreadExecutor();
+
+    static {
+        MoreExecutors.addDelayedShutdownHook(pool, 1, TimeUnit.MILLISECONDS);
     }
 
     public static void debug(String msg) {
-        log.debug(msg);
+        exec(() -> log.debug(msg));
     }
 
     public static void debug(String pattern, Object... args) {
-        log.debug(MessageFormat.format(pattern, args));
+        exec(() -> log.debug(MessageFormat.format(pattern, args)));
     }
 
     public static void info(String msg) {
-        log.info(msg);
+        exec(() -> log.info(msg));
     }
 
     public static void info(String pattern, Object... args) {
-        log.info(MessageFormat.format(pattern, args));
+        exec(() -> log.info(MessageFormat.format(pattern, args)));
     }
 
     public static void warn(String msg) {
-        log.warn(msg);
+        exec(() -> log.warn(msg));
     }
 
     public static void warn(String pattern, Object... args) {
-        log.warn(MessageFormat.format(pattern, args));
+        exec(() -> log.warn(MessageFormat.format(pattern, args)));
     }
 
     public static void warn(Throwable e) {
-        log.warn(e);
+        exec(() -> log.warn(e));
     }
 
     public static void warn(Throwable e, String pattern, Object... args) {
-        log.warn(MessageFormat.format(pattern, args), e);
+        exec(() -> log.warn(MessageFormat.format(pattern, args), e));
     }
 
     public static void error(String msg) {
-        log.error(msg);
+        exec(() -> log.error(msg));
     }
 
     public static void error(String pattern, Object... args) {
-        log.error(MessageFormat.format(pattern, args));
+        exec(() -> log.error(MessageFormat.format(pattern, args)));
     }
 
     public static void error(Throwable e) {
-        log.error(e);
+        exec(() -> log.error(e));
     }
 
     public static void error(Throwable e, String pattern, Object... args) {
-        log.error(MessageFormat.format(pattern, args), e);
+        exec(() -> log.error(MessageFormat.format(pattern, args), e));
+    }
+
+    private static void exec(Runnable runnable) {
+        try {
+            pool.submit(runnable);
+        }
+        catch (RejectedExecutionException e) {
+            runnable.run();
+        }
     }
 }
