f17c58a72beb38ed707757ee91874764606da154
Yoann <y.vernageau@gmail.com>
2016-11-29 14:58:25 +0100

    replace comparison operators

--- .script/publish_javadoc.sh
+++ .script/publish_javadoc.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -ev
-if [ "$TRAVIS_REPO_SLUG" == "atlanmod/NeoEMF" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+if [ "$TRAVIS_REPO_SLUG" = "atlanmod/NeoEMF" ] && [ "$TRAVIS_JDK_VERSION" = "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
   echo -e "Publishing javadoc...\n"
+
   cp -R target/site/apidocs $HOME/.neoemf/apidocs
   cd $HOME
+
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} gh-pages > /dev/null
+
   cd gh-pages
+
   git rm -rf ./apidocs
   cp -Rf $HOME/.neoemf/apidocs ./apidocs
+
   git add -f .
   git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
+
   echo -e "Published Javadoc to gh-pages.\n"
 fi
