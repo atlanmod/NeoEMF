a69bdf4c9464bdd59732908e14d06be621bfd678
Yoann <y.vernageau@gmail.com>
2016-11-30 19:59:44 +0100

    - rename directories according to the Maven artifact - move configuration files

---
+++ neoemf-core/src/main/java/fr/inria/atlanmod/neoemf/datastore/InvalidOptionsException.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2013-2016 Atlanmod INRIA LINA Mines Nantes.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Atlanmod INRIA LINA Mines Nantes - initial API and implementation
+ */
+
+package fr.inria.atlanmod.neoemf.datastore;
+
+public class InvalidOptionsException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidOptionsException() {
+    }
+
+    public InvalidOptionsException(String message) {
+        super(message);
+    }
+
+    public InvalidOptionsException(Throwable t) {
+        super(t);
+    }
+
+    public InvalidOptionsException(String message, Throwable t) {
+        super(message, t);
+    }
+}
+
