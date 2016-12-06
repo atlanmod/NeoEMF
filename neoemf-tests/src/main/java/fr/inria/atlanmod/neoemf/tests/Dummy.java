2af5ff7aa45707ae8f8d3fb7cde6318483a228a4
Yoann <y.vernageau@gmail.com>
2016-12-06 16:04:53 +0100

    add a Dummy class in `tests` to fix Maven deployment

---
+++ neoemf-tests/src/main/java/fr/inria/atlanmod/neoemf/tests/Dummy.java
@@ -0,0 +1,12 @@
+package fr.inria.atlanmod.neoemf.tests;
+
+/**
+ * This is a dummy class : <b>DON'T USE IT</b> !
+ * <p/>
+ * This class is here to ensure a valid deployment in Maven Central.
+ */
+final class Dummy {
+
+    private Dummy() {
+    }
+}
