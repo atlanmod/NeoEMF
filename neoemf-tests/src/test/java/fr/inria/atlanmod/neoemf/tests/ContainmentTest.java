bec10357f5762ba15f23ffff59181ff3cd4e2157
Yoann <y.vernageau@gmail.com>
2017-03-27 20:13:54 +0200

    add some assertions

--- neoemf-tests/src/test/java/fr/inria/atlanmod/neoemf/tests/ContainmentTest.java
+++ neoemf-tests/src/test/java/fr/inria/atlanmod/neoemf/tests/ContainmentTest.java
@@ -12,7 +12,6 @@
 package fr.inria.atlanmod.neoemf.tests;
 
 import fr.inria.atlanmod.neoemf.Tags;
-import fr.inria.atlanmod.neoemf.data.store.Store;
 import fr.inria.atlanmod.neoemf.resource.PersistentResource;
 import fr.inria.atlanmod.neoemf.tests.models.mapSample.AbstractPackContentComment;
 import fr.inria.atlanmod.neoemf.tests.models.mapSample.Pack;
@@ -39,22 +38,32 @@ public class ContainmentTest extends AbstractBackendTest {
 
         resource.getContents().add(p1);
 
+        assertThat(p1.resource()).isSameAs(resource);
+        assertThat(p1.eInternalContainer()).isNull();
+
         Pack p2 = EFACTORY.createPack();
         p2.setName("P2");
+
         PackContent pc1 = EFACTORY.createPackContent();
 
         Pack p3 = EFACTORY.createPack();
         p3.setName("P3");
+
         p2.getPacks().add(p3);
+
+        assertThat(p3.eInternalContainer()).isEqualTo(p2);
+
         pc1.setName("PC1");
+
         p3.getOwnedContents().add(pc1);
+
+        assertThat(pc1.eInternalContainer()).isEqualTo(p3);
 
         p1.getPacks().add(p2);
 
-        assertThat(p1.eStore()).isInstanceOf(Store.class);
-        assertThat(p2.eStore()).isInstanceOf(Store.class);
-        assertThat(p3.eStore()).isInstanceOf(Store.class);
-        assertThat(pc1.eStore()).isInstanceOf(Store.class);
+        assertThat(p2.eInternalContainer()).isEqualTo(p1);
+
+        assertThat(resource.getAllContents()).hasSize(4);
 
         assertThat(p1.resource()).isSameAs(resource);
         assertThat(p2.resource()).isSameAs(resource);
@@ -72,11 +81,17 @@ public class ContainmentTest extends AbstractBackendTest {
 
         resource.getContents().add(p1);
 
+        assertThat(p1.resource()).isSameAs(resource);
+        assertThat(p1.eInternalContainer()).isNull();
+
         Pack p2 = EFACTORY.createPack();
         p2.setName("P2");
+
         p1.getPacks().add(p2);
+
         PackContent pc1 = EFACTORY.createPackContent();
         pc1.setName("PC1");
+
         p2.getOwnedContents().add(pc1);
 
         AbstractPackContentComment com1 = EFACTORY.createAbstractPackContentComment();
@@ -88,11 +103,9 @@ public class ContainmentTest extends AbstractBackendTest {
         // Then add the element to the resource tree using the containment reference
         pc1.getContainmentNoOppositeRefComment().add(com1);
 
-        assertThat(com1.eStore()).isInstanceOf(Store.class);
         assertThat(com1.resource()).isSameAs(resource);
 
         // Check that the element has a container (it cannot be in the resource if it does not)
-        assertThat(com1.eContainer()).isEqualTo(pc1);
         assertThat(com1.eInternalContainer()).isEqualTo(pc1);
 
         // Check that the element is in the containment reference list of its parent
