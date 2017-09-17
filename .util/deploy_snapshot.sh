4ada5c25824ca0ff79cfccb4cecb8cd126d0d75b
Yoann Vernageau <y.vernageau@gmail.com>
2017-09-17 08:20:53 +0200

    review Travis configuration

--- .util/deploy_snapshot.sh
+++ .util/deploy_snapshot.sh
@@ -29,7 +29,7 @@ else
 
     echo "Publishing Maven snapshot..."
 
-    mvn -B -q clean source:jar-no-fork javadoc:jar install deploy --settings=".util/settings.xml" -DskipTests
+    mvn -B -q -DskipTests clean source:jar-no-fork javadoc:jar install deploy --settings=".util/settings.xml"
 
     echo "Maven snapshot published."
 fi
