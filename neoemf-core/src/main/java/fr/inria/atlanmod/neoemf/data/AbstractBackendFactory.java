59dbf703bf7172af11808971b291da56d8816690
Yoann Vernageau <y.vernageau@gmail.com>
2018-01-03 18:48:59 +0100

    split methods in `AbstractBackendFactory`

--- neoemf-core/src/main/java/fr/inria/atlanmod/neoemf/data/AbstractBackendFactory.java
+++ neoemf-core/src/main/java/fr/inria/atlanmod/neoemf/data/AbstractBackendFactory.java
@@ -15,6 +15,7 @@ import fr.inria.atlanmod.neoemf.data.mapping.AbstractMapperFactory;
 
 import org.eclipse.emf.common.util.URI;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -23,6 +24,8 @@ import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import static fr.inria.atlanmod.commons.Preconditions.checkArgument;
+import static fr.inria.atlanmod.commons.Preconditions.checkNotNull;
 import static java.util.Objects.isNull;
 
 /**
@@ -38,49 +41,79 @@ public abstract class AbstractBackendFactory<C extends Config> extends AbstractM
     protected AbstractBackendFactory() {
     }
 
+    /**
+     * Creates a {@link Path} from the given {@code uri}.
+     *
+     * @param uri the URI to convert
+     *
+     * @return a new file path
+     */
+    @Nonnull
+    private static Path uriToPath(URI uri) {
+        return Paths.get(uri.toFileString());
+    }
+
+    /**
+     * Creates a {@link URL} from the given {@code uri}.
+     *
+     * @param uri the URI to convert
+     *
+     * @return a new URL
+     *
+     * @throws MalformedURLException if an error occurs during the {@link URL} creation
+     */
+    @Nonnull
+    private static URL uriToUrl(URI uri) throws MalformedURLException {
+        final String protocol = "http";
+        final String delimiter = "_";
+
+        String path = uri.segmentsList().stream()
+                .map(s -> s.replaceAll("-", delimiter))
+                .collect(Collectors.joining(delimiter));
+
+        return new URL(protocol, uri.host(), isNull(uri.port()) ? -1 : Integer.parseInt(uri.port()), path);
+    }
+
     @Nonnull
     @Override
     @SuppressWarnings("unchecked")
     public Backend createBackend(URI uri, ImmutableConfig baseConfig) {
-        Backend newBackend;
+        checkNotNull(uri, "uri");
+        checkNotNull(baseConfig, "baseConfig");
+        checkArgument(uri.isFile() || uri.isHierarchical(), "URI '%s' must be either file-based or hierarchical", uri.toString());
 
         try {
-            if (uri.isFile()) {
-                Path baseDirectory = Paths.get(uri.toFileString());
-
-                // Merge and check conflicts between the two configurations, or create a new default configuration
-                C mergedConfig = (C) Config.load(baseDirectory)
-                        .orElseGet(() -> Config.forName(name()))
-                        .merge(baseConfig);
-
-                newBackend = createLocalBackend(baseDirectory, mergedConfig);
-
-                // Save the configuration next to the created database
-                mergedConfig.save(baseDirectory);
-            }
-            else if (uri.isHierarchical()) {
-                final String targetDelimiter = "_";
-                String path = uri.segmentsList().stream()
-                        .map(s -> s.replaceAll("-", targetDelimiter))
-                        .collect(Collectors.joining(targetDelimiter));
-
-                URL url = new URL("http", uri.host(), isNull(uri.port()) ? -1 : Integer.parseInt(uri.port()), path);
-
-                newBackend = createRemoteBackend(url, (C) baseConfig);
-            }
-            else {
-                throw new IllegalArgumentException(String.format("URI '%s' must be either file-based or hierarchical", uri.toString()));
-            }
-        }
-        catch (InvalidBackendException e) {
-            throw e;
-        }
-        catch (ClassCastException e) {
-            throw new IllegalArgumentException(String.format("Unknown configuration of type %s", baseConfig.getClass().getName()), e);
+            return uri.isFile()
+                    ? createLocalBackend(uri, baseConfig)
+                    : createRemoteBackend(uri, baseConfig);
         }
         catch (Exception e) {
-            throw new InvalidBackendException("Unable to open the database", e);
+            throw new InvalidBackendException("Unable to create/open the database", e);
         }
+    }
+
+    /**
+     * Creates a new file-based {@link Backend}.
+     *
+     * @param uri        the {@link URI} where to store the back-end
+     * @param baseConfig the base configuration that defines the behaviour of the back-end
+     *
+     * @return a new back-end
+     *
+     * @throws Exception if an error occurs during the back-end initialization
+     */
+    @Nonnull
+    @SuppressWarnings("unchecked")
+    private Backend createLocalBackend(URI uri, ImmutableConfig baseConfig) throws Exception {
+        Path directory = uriToPath(uri);
+
+        // Merge and check conflicts between the two configurations, or create a new default mutable configuration
+        C mergedConfig = (C) Config.load(directory).orElseGet(() -> Config.forName(name())).merge(baseConfig);
+
+        Backend newBackend = createLocalBackend(directory, mergedConfig);
+
+        // Save the configuration next to the created database
+        mergedConfig.save(directory);
 
         return newBackend;
     }
@@ -96,11 +129,32 @@ public abstract class AbstractBackendFactory<C extends Config> extends AbstractM
      * @return a new back-end
      *
      * @throws Exception if an error occurs during the back-end initialization
-     * @see #createBackend(URI, ImmutableConfig)
      */
     @Nonnull
     protected Backend createLocalBackend(Path directory, C config) throws Exception {
         throw new UnsupportedOperationException(String.format("%s only supports file-based URIs", getClass().getSimpleName()));
+    }
+
+    /**
+     * Creates a new server-based {@link Backend}.
+     *
+     * @param uri        the {@link URI} where to store the back-end
+     * @param baseConfig the base configuration that defines the behaviour of the back-end
+     *
+     * @return a new back-end
+     *
+     * @throws Exception if an error occurs during the back-end initialization
+     */
+    @Nonnull
+    @SuppressWarnings("unchecked")
+    // TODO Save the configuration in a default NeoEMF directory ('${user.home}/.neoemf/conf/' for example)
+    private Backend createRemoteBackend(URI uri, ImmutableConfig baseConfig) throws Exception {
+        URL url = uriToUrl(uri);
+
+        // Create a new default mutable configuration
+        C mergedConfig = (C) Config.forName(name()).merge(baseConfig);
+
+        return createRemoteBackend(url, mergedConfig);
     }
 
     /**
@@ -114,7 +168,6 @@ public abstract class AbstractBackendFactory<C extends Config> extends AbstractM
      * @return a new back-end
      *
      * @throws Exception if an error occurs during the back-end initialization
-     * @see #createBackend(URI, ImmutableConfig)
      */
     @Nonnull
     protected Backend createRemoteBackend(URL url, C config) throws Exception {
