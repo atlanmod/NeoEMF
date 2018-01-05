3465ed02ec7e55233f34d493f23dd568fe030d37
Yoann Vernageau <y.vernageau@gmail.com>
2018-01-05 15:58:15 +0100

    update copyrights

--- neoemf-core/src/main/java/fr/inria/atlanmod/neoemf/bind/FactoryBinding.java
+++ neoemf-core/src/main/java/fr/inria/atlanmod/neoemf/bind/FactoryBinding.java
@@ -1,17 +1,5 @@
 /*
- * Copyright (c) 2013-2017 Atlanmod, Inria, LS2N, and IMT Nantes.
- *
- * All rights reserved. This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License v2.0 which accompanies
- * this distribution, and is available at https://www.eclipse.org/legal/epl-2.0/
- *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License, v. 2.0 are satisfied: GNU General Public License, version 3.
- */
-
-/*
- * Copyright (c) 2013-2017 Atlanmod, Inria, LS2N, and IMT Nantes.
+ * Copyright (c) 2013-2018 Atlanmod, Inria, LS2N, and IMT Nantes.
  *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v2.0 which accompanies
