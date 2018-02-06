f84f566f047fd5babe48f86e385c563444f1b7f1
Yoann Vernageau <y.vernageau@gmail.com>
2018-02-06 14:49:13 +0100

    some minor optimizations in I/O

--- neoemf-io/src/main/java/fr/inria/atlanmod/neoemf/io/processor/XPathResolver.java
+++ neoemf-io/src/main/java/fr/inria/atlanmod/neoemf/io/processor/XPathResolver.java
@@ -8,29 +8,19 @@
 
 package fr.inria.atlanmod.neoemf.io.processor;
 
-import fr.inria.atlanmod.commons.cache.Cache;
-import fr.inria.atlanmod.commons.cache.CacheBuilder;
-import fr.inria.atlanmod.commons.primitive.Strings;
 import fr.inria.atlanmod.neoemf.core.Id;
+import fr.inria.atlanmod.neoemf.core.IdProvider;
 import fr.inria.atlanmod.neoemf.io.Handler;
 import fr.inria.atlanmod.neoemf.io.bean.BasicElement;
 import fr.inria.atlanmod.neoemf.io.bean.BasicReference;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import static fr.inria.atlanmod.commons.Preconditions.checkNotNull;
-import static fr.inria.atlanmod.commons.Preconditions.checkState;
 import static fr.inria.atlanmod.neoemf.io.util.XPathConstants.END_EXPR;
 import static fr.inria.atlanmod.neoemf.io.util.XPathConstants.INDEX_SEPARATOR;
 import static fr.inria.atlanmod.neoemf.io.util.XPathConstants.START_ELT;
@@ -53,12 +43,10 @@ public class XPathResolver extends AbstractProcessor<Handler> {
     private static final Pattern PATTERN_NO_INDEX = Pattern.compile("((@\\w+)(?!\\.\\d+)(/|\\z))", Pattern.UNICODE_CASE);
 
     /**
-     * A cache that holds recently generated {@link Id}s.
+     * The default {@link IdProvider} to use for generating identifiers.
      */
     @Nonnull
-    private final Cache<String, Id> hashCache = CacheBuilder.builder()
-            .softValues()
-            .build();
+    private final IdProvider idProvider = Id.getProvider();
 
     /**
      * The XPath structure.
@@ -125,9 +113,6 @@ public class XPathResolver extends AbstractProcessor<Handler> {
 
     @Override
     public void onComplete() {
-        hashCache.invalidateAll();
-        hashCache.cleanUp();
-
         notifyComplete();
     }
 
@@ -141,40 +126,29 @@ public class XPathResolver extends AbstractProcessor<Handler> {
             return;
         }
 
+        Id id;
         String rawId = element.rawId();
 
         // An element has no identifier if it's not resolved
         if (ignore || nonNull(rawId)) {
-            checkState(nonNull(rawId), "raw id must be set");
-            Id id;
-            try {
-                id = Id.getProvider().fromHexString(rawId);
-            }
-            catch (IllegalArgumentException e) {
-                id = generateId(rawId);
-            }
-            element.id(id);
+            checkNotNull(rawId, "raw id must be set");
+            id = getOrGenerateId(rawId);
         }
         else {
-            // Processes the raw identifier from the path of the element in XML tree
-            String path = paths.path(element.name());
-
             // Increments the number of occurrence for this path
-            int count = paths.createOrIncrement(element.name());
-
+            // Processes the raw identifier from the path of the element in XML tree
             // Defines the raw identifier as '<path>.<index>'
-            path = path + INDEX_SEPARATOR + count;
+            final String path = paths.path(element.name()) + INDEX_SEPARATOR + paths.createOrIncrement(element.name());
 
             // Defines the XPath start of all elements from the root element
             if (isNull(expressionStart)) {
                 expressionStart = path + START_ELT;
             }
 
-            // Defines the new identifier of the element
-            element.id(generateId(path));
+            id = generateId(path);
         }
 
-        element.rawId(null);
+        element.id(id).rawId(null);
     }
 
     /**
@@ -187,18 +161,12 @@ public class XPathResolver extends AbstractProcessor<Handler> {
             return;
         }
 
+        Id referencedId;
         String rawValue = reference.rawValue();
-        checkState(nonNull(rawValue), "raw value must be set");
+        checkNotNull(rawValue, "raw value must be set");
 
         if (ignore || !rawValue.startsWith(START_EXPR) && !rawValue.startsWith(START_ELT)) {
-            Id value;
-            try {
-                value = Id.getProvider().fromHexString(rawValue);
-            }
-            catch (IllegalArgumentException e) {
-                value = generateId(rawValue);
-            }
-            reference.value(value);
+            referencedId = getOrGenerateId(rawValue);
         }
         else {
             // Replace the start of the given reference "//@" -> "/@<rootname>.<index>"
@@ -215,11 +183,27 @@ public class XPathResolver extends AbstractProcessor<Handler> {
                 path = path.substring(0, path.length() - END_EXPR.length());
             }
 
-            // Defines the new identifier of the reference
-            reference.value(generateId(path));
+            referencedId = generateId(path);
         }
 
-        reference.rawValue(null);
+        reference.value(referencedId).rawValue(null);
+    }
+
+    /**
+     * Retrieves the {@link Id} associated to the given {@code baseValue}.
+     *
+     * @param baseValue the string value of the identifier
+     *
+     * @return the identifier
+     */
+    @Nonnull
+    private Id getOrGenerateId(String baseValue) {
+        try {
+            return idProvider.fromHexString(baseValue);
+        }
+        catch (IllegalArgumentException e) {
+            return generateId(baseValue);
+        }
     }
 
     /**
@@ -231,202 +215,6 @@ public class XPathResolver extends AbstractProcessor<Handler> {
      */
     @Nonnull
     private Id generateId(String baseValue) {
-        return hashCache.get(baseValue, v -> Id.getProvider().generate(v));
-    }
-
-    /**
-     * A structure representing an XPath.
-     * <p>
-     * It takes the form of a tree that contains nodes: The XPath is updated for each addition/deletion of a node, and
-     * only the nodes representing the current XPath are hold. This allows to treat an XPath without keeping all the
-     * elements in memory.
-     */
-    @ParametersAreNonnullByDefault
-    private static class XPathTree {
-
-        /**
-         * The root of this tree.
-         * <p>
-         * It does not represent the root node path.
-         */
-        @Nonnull
-        private final XPathNode root = new XPathNode(Strings.EMPTY);
-
-        /**
-         * A LIFO that holds the current XPath.
-         */
-        @Nonnull
-        private final Deque<XPathNode> currentPath = new ArrayDeque<>();
-
-        /**
-         * Gets the XPath of the current element.
-         *
-         * @param name the name of the last element of the path
-         *
-         * @return a string representing the XPath of the element
-         */
-        @Nonnull
-        public String path(String name) {
-            checkNotNull(name, "name");
-
-            StringBuilder str = new StringBuilder(START_ELT);
-            boolean first = true;
-            for (XPathNode node : currentPath) {
-                if (!first) {
-                    str.append(START_ELT);
-                }
-                str.append(node.name()).append(INDEX_SEPARATOR).append(node.index());
-                first = false;
-            }
-            str.append(first ? Strings.EMPTY : START_ELT).append(name);
-            return str.toString();
-        }
-
-        /**
-         * Creates or increments the node with the specified {@code name}. If a child node has already the {@code name},
-         * its index will be incremented, otherwise a new child will be added with {@code index = 0}.
-         *
-         * @param name the name of the node
-         *
-         * @return the index of the node
-         */
-        @Nonnegative
-        public int createOrIncrement(String name) {
-            // Get the current Node or the root Node if no element exists
-            XPathNode node = currentPath.isEmpty() ? root : currentPath.getLast();
-
-            if (node.hasChild(name)) {
-                // Try to get and increment the node if it exists
-                node = node.child(name);
-                node.incrementIndex();
-            }
-            else {
-                // The node doesn't exist : we create him
-                node = node.child(new XPathNode(name));
-            }
-
-            // Define the node as the current node in path
-            currentPath.addLast(node);
-            return node.index();
-        }
-
-        /**
-         * Removes the last child and, recursively its children, from this tree.
-         */
-        public void clearLast() {
-            currentPath.removeLast().clear();
-        }
-
-        /**
-         * A node of the {@link XPathTree}.
-         * <p>
-         * A node is a segment of an XPath, for example {@code .../@name.index/...}.
-         */
-        @ParametersAreNonnullByDefault
-        private static class XPathNode {
-
-            /**
-             * The name of this node.
-             */
-            @Nonnull
-            private final String name;
-
-            /**
-             * A map that holds all children of this node, identified by their name.
-             */
-            @Nonnull
-            private final Map<String, XPathNode> children = new HashMap<>();
-
-            /**
-             * The index of this node.
-             */
-            @Nonnull
-            private final AtomicInteger index = new AtomicInteger();
-
-            /**
-             * Constructs a new {@code XPathNode} with the given {@code name}.
-             *
-             * @param name the name of this node
-             */
-            public XPathNode(String name) {
-                this.name = checkNotNull(name, "name");
-            }
-
-            /**
-             * Returns the name of this node.
-             *
-             * @return the name
-             */
-            @Nonnull
-            public String name() {
-                return name;
-            }
-
-            /**
-             * Returns the index of this node.
-             *
-             * @return the index
-             */
-            @Nonnegative
-            public int index() {
-                return index.get();
-            }
-
-            /**
-             * Increments the index of this node.
-             */
-            public void incrementIndex() {
-                index.incrementAndGet();
-            }
-
-            /**
-             * Returns the node which has the specified {@code name} among the children of this node.
-             *
-             * @param id he identifier of the sought child
-             *
-             * @return the child node if it exists
-             *
-             * @throws NoSuchElementException if no child has the given {@code name}
-             */
-            @Nonnull
-            public XPathNode child(String id) {
-                XPathNode child = children.get(id);
-                if (isNull(child)) {
-                    throw new NoSuchElementException("No such element '" + id + "' in the element '" + this.name + '\'');
-                }
-                return child;
-            }
-
-            /**
-             * Adds a {@code child} to this node.
-             *
-             * @param child the node to add as child
-             *
-             * @return the given {@code child} (for chaining)
-             */
-            @Nonnull
-            public XPathNode child(XPathNode child) {
-                children.put(child.name(), child);
-                return child;
-            }
-
-            /**
-             * Removes all children of this node.
-             */
-            public void clear() {
-                children.clear();
-            }
-
-            /**
-             * Defines whether this node has a child with the specified {@code name}.
-             *
-             * @param name the name of the sought child
-             *
-             * @return {@code true} if a child node has the specified {@code name}
-             */
-            public boolean hasChild(String name) {
-                return nonNull(children.get(name));
-            }
-        }
+        return idProvider.generate(baseValue);
     }
 }
