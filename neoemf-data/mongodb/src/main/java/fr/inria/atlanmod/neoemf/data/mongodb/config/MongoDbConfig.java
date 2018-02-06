30013eb4146849a06c460df3badaaee328f59be4
natinusala <natinusala@gmail.com>
2018-02-06 16:17:42 +0100

    added mongodb skeleton

---
+++ neoemf-data/mongodb/src/main/java/fr/inria/atlanmod/neoemf/data/mongodb/config/MongoDbConfig.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2013-2018 Atlanmod, Inria, LS2N, and IMT Nantes.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v2.0 which accompanies
+ * this distribution, and is available at https://www.eclipse.org/legal/epl-2.0/
+ */
+
+package fr.inria.atlanmod.neoemf.data.mongodb.config;
+
+import fr.inria.atlanmod.neoemf.bind.FactoryBinding;
+import fr.inria.atlanmod.neoemf.config.BaseConfig;
+
+import fr.inria.atlanmod.neoemf.data.mongodb.MongoDbBackendFactory;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+/**
+ * A {@link fr.inria.atlanmod.neoemf.config.Config} that creates MongoDb specific configuration.
+ * <p>
+ * All features are all optional: configuration can be created using all or none of them.
+ */
+@FactoryBinding(factory = MongoDbBackendFactory.class)
+@ParametersAreNonnullByDefault
+public class MongoDbConfig extends BaseConfig<MongoDbConfig> {
+
+    /**
+     * Constructs a new {@code MongoDbConfig}.
+     */
+    protected MongoDbConfig() {
+        withDefault();
+
+        // TODO Declare all default values
+    }
+
+    /**
+     * Constructs a new {@code MongoDbConfig} instance with default settings.
+     *
+     * @return a new configuration
+     */
+    @Nonnull
+    public static MongoDbConfig newConfig() {
+        return new MongoDbConfig();
+    }
+
+    /**
+     * Defines the mapping to use for the created {@link fr.inria.atlanmod.neoemf.data.mongodb.MongoDbBackend}.
+     *
+     * @return this configuration (for chaining)
+     */
+    protected MongoDbConfig withDefault() {
+        return setMappingWithCheck("fr.inria.atlanmod.neoemf.data.mongodb.DefaultMongoDbBackend", false);
+    }
+
+    // TODO Add mapping declarations
+
+    // TODO Add methods specific to your database
+}
---
+++ neoemf-data/mongodb/src/test/java/fr/inria/atlanmod/neoemf/data/mongodb/context/MongoDbContext.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2013-2018 Atlanmod, Inria, LS2N, and IMT Nantes.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v2.0 which accompanies
+ * this distribution, and is available at https://www.eclipse.org/legal/epl-2.0/
+ */
+
+package fr.inria.atlanmod.neoemf.data.mongodb.context;
+
+import fr.inria.atlanmod.neoemf.config.ImmutableConfig;
+import fr.inria.atlanmod.neoemf.context.AbstractContext;
+import fr.inria.atlanmod.neoemf.context.Context;
+import fr.inria.atlanmod.neoemf.data.BackendFactory;
+
+import fr.inria.atlanmod.neoemf.data.mongodb.MongoDbBackendFactory;
+import fr.inria.atlanmod.neoemf.data.mongodb.config.MongoDbConfig;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+/**
+ * A specific {@link Context} for the MongoDb implementation.
+ */
+@ParametersAreNonnullByDefault
+public abstract class MongoDbContext extends AbstractContext {
+
+    /**
+     * Creates a new {@code BerkeleyDbContext}.
+     *
+     * @return a new context.
+     */
+    @Nonnull
+    public static Context getDefault() {
+        return new MongoDbContext() {
+            @Nonnull
+            @Override
+            public ImmutableConfig config() {
+                return MongoDbConfig.newConfig();
+            }
+        };
+    }
+
+    @Nonnull
+    @Override
+    public String name() {
+        return "MongoDb";
+    }
+
+    @Nonnull
+    @Override
+    public BackendFactory factory() {
+        return MongoDbBackendFactory.getInstance();
+    }
+}
