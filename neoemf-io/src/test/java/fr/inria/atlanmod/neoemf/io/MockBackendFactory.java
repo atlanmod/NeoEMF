6dede6a9d9beca537fbe76a16a861afcdfe73b81
Yoann Vernageau <y.vernageau@gmail.com>
2018-02-10 19:02:56 +0100

    add javadoc on `MockBackendFactory`

--- neoemf-io/src/test/java/fr/inria/atlanmod/neoemf/io/MockBackendFactory.java
+++ neoemf-io/src/test/java/fr/inria/atlanmod/neoemf/io/MockBackendFactory.java
@@ -15,6 +15,7 @@ import fr.inria.atlanmod.neoemf.config.ImmutableConfig;
 import fr.inria.atlanmod.neoemf.data.AbstractBackendFactory;
 import fr.inria.atlanmod.neoemf.data.Backend;
 import fr.inria.atlanmod.neoemf.data.BackendFactory;
+import fr.inria.atlanmod.neoemf.data.BackendFactoryRegistry;
 import fr.inria.atlanmod.neoemf.data.InvalidBackend;
 import fr.inria.atlanmod.neoemf.util.AbstractUriBuilder;
 import fr.inria.atlanmod.neoemf.util.UriBuilder;
@@ -28,17 +29,22 @@ import javax.annotation.ParametersAreNonnullByDefault;
  * A mocked {@link BackendFactory} that always returns the same {@link Backend}.
  * <p>
  * <b>NOTE:</b> This class exists for binding a {@link Config} and {@link UriBuilder} to this {@link BackendFactory}.
- * You need to register the factory you want to use, with {@link fr.inria.atlanmod.neoemf.data.BackendFactoryRegistry#register(String,
- * BackendFactory)}, before any call to {@link BackendFactory#createBackend(URI, ImmutableConfig)}.
+ * You need to register the factory you want to use, with {@link BackendFactoryRegistry#register(String, BackendFactory)},
+ * before any call to {@link BackendFactory#createBackend(URI, ImmutableConfig)}.
  */
 @ParametersAreNonnullByDefault
 public final class MockBackendFactory extends AbstractBackendFactory<MockBackendFactory.MockConfig> {
 
+    /**
+     * The literal description of the factory.
+     */
+    @Nonnull
     private static final String NAME = "mock";
 
     /**
      * The back-end returned by {@link #createBackend(URI, ImmutableConfig)}.
      */
+    @Nonnull
     private final Backend backend;
 
     /**
@@ -50,6 +56,12 @@ public final class MockBackendFactory extends AbstractBackendFactory<MockBackend
         this.backend = backend;
     }
 
+    /**
+     * Returns the instance of this class.
+     *
+     * @return the instance of this class
+     */
+    @Nonnull
     public static BackendFactory getInstance() {
         return new MockBackendFactory(new InvalidBackend("You need to register the instance you want to use with BackendFactoryRegistry.register() first"));
     }
@@ -65,21 +77,37 @@ public final class MockBackendFactory extends AbstractBackendFactory<MockBackend
         return backend;
     }
 
+    /**
+     * A {@link Config} that creates configuration for the {@link MockBackendFactory}.
+     */
     @FactoryBinding(factory = MockBackendFactory.class)
     @ParametersAreNonnullByDefault
-    @SuppressWarnings("unused") // Called dynamically
     public static final class MockConfig extends BaseConfig<MockConfig> {
 
-        public static BaseConfig<MockConfig> newConfig() {
+        /**
+         * Constructs a new {@code MockConfig} instance with default settings.
+         *
+         * @return a new configuration
+         */
+        @Nonnull
+        public static MockConfig newConfig() {
             return new MockConfig();
         }
     }
 
+    /**
+     * A {@link UriBuilder} that creates URIs that reference the {@link MockBackendFactory}.
+     */
     @FactoryBinding(factory = MockBackendFactory.class)
     @ParametersAreNonnullByDefault
-    @SuppressWarnings("unused") // Called dynamically
     public static final class MockUri extends AbstractUriBuilder {
 
+        /**
+         * Creates a new {@link UriBuilder} with the pre-configured scheme.
+         *
+         * @return a new builder
+         */
+        @Nonnull
         public static UriBuilder builder() {
             return new MockUri();
         }
