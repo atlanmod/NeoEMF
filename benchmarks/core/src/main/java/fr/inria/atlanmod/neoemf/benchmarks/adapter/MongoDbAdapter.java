7082d723f61d5fa78ba1db8ea520e4c09c8fa88e
Florent ALAPETITE <florent037@gmail.com>
2018-05-06 20:58:19 +0200

    Changes tot he MongoAdapter + AbstractBackendFactory to handle server URL

--- benchmarks/core/src/main/java/fr/inria/atlanmod/neoemf/benchmarks/adapter/MongoDbAdapter.java
+++ benchmarks/core/src/main/java/fr/inria/atlanmod/neoemf/benchmarks/adapter/MongoDbAdapter.java
@@ -1,11 +1,18 @@
 package fr.inria.atlanmod.neoemf.benchmarks.adapter;
 
 
+import fr.inria.atlanmod.neoemf.config.BaseConfig;
+import fr.inria.atlanmod.neoemf.config.ImmutableConfig;
+import fr.inria.atlanmod.neoemf.data.Backend;
 import fr.inria.atlanmod.neoemf.data.BackendFactory;
+import fr.inria.atlanmod.neoemf.data.mapping.DataMapper;
 import fr.inria.atlanmod.neoemf.data.mongodb.MongoDbBackendFactory;
 import fr.inria.atlanmod.neoemf.data.mongodb.config.MongoDbConfig;
+import fr.inria.atlanmod.neoemf.data.store.StoreFactory;
+import org.eclipse.emf.common.util.URI;
 
 import javax.annotation.Nonnull;
+import java.io.File;
 import java.util.Map;
 
 
@@ -37,4 +44,12 @@ public class MongoDbAdapter extends AbstractNeoAdapter {
         return MongoDbConfig.newConfig().toMap();
     }
 
+    @Override
+    public DataMapper createMapper(File file, ImmutableConfig config) {
+        ImmutableConfig mergedConfig = BaseConfig.newConfig().merge(config).merge(getOptions());
+
+        //TODO Get configuration from file
+        Backend backend = getFactory().createBackend(URI.createURI("neo-mongodb://localhost:27017/testbenchmark"), mergedConfig);
+        return StoreFactory.getInstance().createStore(backend, mergedConfig);
+    }
 }
