abfcd98a2c425c61c56f3e5877bce99f5a4a8ac9
Yoann Vernageau <y.vernageau@gmail.com>
2019-01-21 23:19:53 +0100

    [eclipse] improve backend decorator

--- plugins/eclipse/bundles/ui/src/main/java/fr/inria/atlanmod/neoemf/eclipse/ui/decorator/BackendDirectoryDecorator.java
+++ plugins/eclipse/bundles/ui/src/main/java/fr/inria/atlanmod/neoemf/eclipse/ui/decorator/BackendDirectoryDecorator.java
@@ -14,12 +14,17 @@ import fr.inria.atlanmod.neoemf.eclipse.ui.NeoUIPlugin;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.ILabelDecorator;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Optional;
+
+import static java.util.Objects.nonNull;
 
 /**
  * A {@link ILabelDecorator} that decorates {@link fr.inria.atlanmod.neoemf.data.Backend} directories with an icon.
@@ -31,21 +36,59 @@ public class BackendDirectoryDecorator extends LabelProvider implements ILabelDe
      */
     private static final String ICON_PATH = "icons/full/obj16/neoemf.png";
 
+    /**
+     * The pattern used to append the name of the NeoEMF adapter.
+     */
+    private static final String NAME_PATTERN = " [%s]";
+
     @Override
     public Image decorateImage(Image image, Object element) {
+        final Optional<Path> resourceLocation = getResourceLocation(element);
+
+        if (resourceLocation.isPresent() && Config.exists(resourceLocation.get())) {
+            final ImageDescriptor descriptor = NeoUIPlugin.getImageDescriptor(ICON_PATH);
+            if (nonNull(descriptor)) {
+                return descriptor.createImage();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public String decorateText(String text, Object element) {
+        final Optional<Path> resourceLocation = getResourceLocation(element);
+
+        if (resourceLocation.isPresent()) {
+            try {
+                final Optional<Config> config = Config.load(resourceLocation.get());
+                if (config.isPresent()) {
+                    final String name = config.get().getName();
+                    return text + String.format(NAME_PATTERN, name);
+                }
+            }
+            catch (IOException e) {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the resource location from the given {@code element}.
+     *
+     * @param element the element whose image is being decorated
+     *
+     * @return an {@link Optional} containing the resource location, or {@link Optional#empty()} if the specified {@code
+     * folder} does not represents a directory
+     */
+    private Optional<Path> getResourceLocation(Object element) {
         return Optional.ofNullable(element)
                 .filter(IFolder.class::isInstance)
                 .map(IFolder.class::cast)
                 .map(IResource::getLocation)
                 .map(IPath::toFile)
-                .map(File::toPath)
-                .filter(Config::exists)
-                .map(s -> NeoUIPlugin.getImageDescriptor(ICON_PATH).createImage())
-                .orElse(null);
-    }
-
-    @Override
-    public String decorateText(String text, Object element) {
-        return null;
+                .map(File::toPath);
     }
 }
