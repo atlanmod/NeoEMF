f0b864a7d3f795ead82dc0e4d831f28ad1350e2e
Yoann Vernageau <y.vernageau@gmail.com>
2019-01-25 01:06:17 +0100

    [core] reduce the average number of entries in ChronicleMaps

--- neoemf-core/src/main/java/fr/inria/atlanmod/neoemf/data/im/AbstractInMemoryBackend.java
+++ neoemf-core/src/main/java/fr/inria/atlanmod/neoemf/data/im/AbstractInMemoryBackend.java
@@ -211,7 +211,7 @@ public abstract class AbstractInMemoryBackend extends AbstractBackend implements
         /**
          * The estimated number of entries in maps.
          */
-        protected static final long ENTRIES = (long) Math.pow(2, 20);
+        protected static final long ENTRIES = (long) Math.pow(2, 16);
 
         /**
          * The estimated size of an {@link Id}.
