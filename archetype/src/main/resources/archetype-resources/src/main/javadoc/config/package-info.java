cb06decb1b5a674165c8b09e2501b929ec1ae393
Yoann Vernageau <y.vernageau@gmail.com>
2019-03-19 23:24:58 +0100

    [archetype] remove copyright

--- archetype/src/main/resources/archetype-resources/src/main/javadoc/config/package-info.java
+++ archetype/src/main/resources/archetype-resources/src/main/javadoc/config/package-info.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) 2013-2018 Atlanmod, Inria, LS2N, and IMT Nantes.
- *
- * All rights reserved. This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License v2.0 which accompanies
- * this distribution, and is available at https://www.eclipse.org/legal/epl-2.0/
- */
-
 /**
  * Provides configuration classes to define specific behaviors of ${databaseName} data persistence.
  */
